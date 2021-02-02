904b694faa57974abadf4a5ee88fef2bbfba9f5d
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-02-02 18:22:37 +0100

    ~~~

---
+++ OOP/Inheritance.py
@@ -0,0 +1,28 @@
+# -------------------------------------------First Class------------------------------------
+class Animal:
+    def __init__(self):
+        print("Animal created")
+
+    def whoAmI(self):
+        print("Animal")
+
+    def eat(self):
+        print("Eating")
+
+# --------------------------------------------Second Class-----------------------------------
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog created")
+
+    def whoAmI(self):
+        print("Dog")
+
+    def bark(self):
+        print("Woof!")
+
+
+d = Dog()
+d.whoAmI()
+d.eat()
+d.bark()
