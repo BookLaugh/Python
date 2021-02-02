f980495096e8890c5122b4fa15fe519ccada7a22
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-02-02 16:14:46 +0100

    &&&

---
+++ OOP/Problem1.py
@@ -0,0 +1,30 @@
+import math
+class Line():
+
+    def __init__(self,coor1,coor2):
+
+        self.coor1 = coor1
+        self.coor2 = coor2
+
+    def distance(self):
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+
+        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    def slope(self):
+        x1, y1 = self.coor1
+        x2, y2 = self.coor2
+
+        return (y2-y1) / (x2-x1)
+
+
+coordinator1 = (3,2)
+coordinator2 = (8,10)
+li = Line(coordinator1,coordinator2)
+
+li.distance()
+li.slope()
+
+
+
