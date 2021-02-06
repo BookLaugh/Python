cf13ca181e3aa8f4a4b14eee7aab769503db3e99
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-02-06 19:09:28 +0100

    ~~~

---
+++ Errors & Exceptions/pylint.py
@@ -0,0 +1,16 @@
+'''
+A very simple Script
+'''
+
+def just():
+    '''
+    A ver simple Function
+    '''
+    num1 = 9
+    num2 = 7
+    print(num1)
+    print(num2)
+    print(num1+num2)
+just()
+
+# To make it run do ---- pylint pylin.py -r y (your_file_name) ------
