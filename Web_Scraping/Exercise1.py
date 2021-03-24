a9f0fcf4f76a593c35b459b09eb060c2a21ac93f
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-03-24 16:42:28 +0100

    Create Exercise1.py

---
+++ Web_Scraping/Exercise1.py
@@ -0,0 +1,16 @@
+
+# TASK: Get the names of all the authors on the first page.
+
+import bs4
+
+result = requests.get('https://quotes.toscrape.com/page/1/')
+soup = bs4.BeautifulSoup(result.text,'lxml')
+
+authors = set()
+lst = soup.select('.author')
+
+for name in lst:
+    authors.add(name.text)
+
+for index,name in enumerate(authors,1):
+    print(index,name)
