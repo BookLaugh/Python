29f7b45b7b6ef85666a3d727e526e809ca1dc84e
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-03-24 16:47:46 +0100

    Update Exercise1.py

--- Web_Scraping/Exercise1.py
+++ Web_Scraping/Exercise1.py
@@ -1,6 +1,7 @@
-
 # TASK: Get the names of all the authors on the first page.
+# NOTE: Make sure pip install those libraries
 
+import requests
 import bs4
 
 result = requests.get('https://quotes.toscrape.com/page/1/')
