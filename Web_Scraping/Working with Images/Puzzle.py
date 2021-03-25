c0193393a8ac0e88bfc90b6b3c86244b35d0d532
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-03-25 12:09:47 +0100

    Create Puzzle.py

---
+++ Web_Scraping/Working with Images/Puzzle.py
@@ -0,0 +1,15 @@
+# NOTE: First make sure to pip install pillow
+
+from PIL import Image
+
+matrix = Image.open("word_matrix.png")
+mask = Image.open("mask.png")
+
+# matrix.paste(mask,(0,0),mask)
+# matrix.show()
+new_mask = mask.resize((1015,559))
+# new_mask.show()
+new_mask.putalpha(200)
+
+matrix.paste(new_mask,(0,0),new_mask)
+matrix.show()
