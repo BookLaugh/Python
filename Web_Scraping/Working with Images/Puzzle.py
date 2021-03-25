ea612cec10a11a36249cfc75ed9aff3c4174e663
Ulan <74649775+Ulihonest@users.noreply.github.com>
2021-03-25 12:11:55 +0100

    Update Puzzle.py

--- Web_Scraping/Working with Images/Puzzle.py
+++ Web_Scraping/Working with Images/Puzzle.py
@@ -5,7 +5,7 @@ from PIL import Image
 matrix = Image.open("word_matrix.png")
 mask = Image.open("mask.png")
 
-# matrix.paste(mask,(0,0),mask)
+matrix.paste(mask,(0,0),mask)
 # matrix.show()
 new_mask = mask.resize((1015,559))
 # new_mask.show()
@@ -13,3 +13,5 @@ new_mask.putalpha(200)
 
 matrix.paste(new_mask,(0,0),new_mask)
 matrix.show()
+
+matrix.save("Puzzle.png")
